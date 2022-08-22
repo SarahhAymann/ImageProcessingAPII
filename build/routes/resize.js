795b0c6f9f2dad63a5d1488c0e1dd3d64a70a57b
SarahhAymann <sarahayman131@gmail.com>
2022-08-22 12:39:45 +0200

    Error Handling added

--- build/routes/resize.js
+++ build/routes/resize.js
@@ -15,18 +15,37 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const express_1 = __importDefault(require("express"));
 const imageresize_1 = __importDefault(require("../imageresize"));
+const fs_2 = require("fs");
 const resize = express_1.default.Router();
 resize.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const fileName = req.query.filename;
     const Width = parseInt(req.query.width);
     const Height = parseInt(req.query.height);
+    const images = yield fs_2.promises.readdir(`images`);
+    const dir = images.find((file) => file === fileName);
+    if (!dir) {
+        res.status(400).json({
+            message: 'Wrong file name  '
+        });
+        return;
+    }
+    if (!fileName) {
+        res.status(400).json({
+            message: 'Mandatory field: name is missing. '
+        });
+        return;
+    }
+    if (!Width || !Height) {
+        res.status(400).json({
+            message: 'Mandatory field: Dimisnions are missing or incorrect. '
+        });
+        return;
+    }
     const newImageSize = imageresize_1.default.resizePath(fileName, Width, Height);
     if (!fs_1.default.existsSync(newImageSize)) {
         yield imageresize_1.default.resizeImage(fileName, Width, Height);
     }
     fs_1.default.readFile(newImageSize, function (err, data) {
-        if (err)
-            throw err;
         res.writeHead(200);
         res.end(data, 'binary');
     });
--- src/routes/resize.ts
+++ src/routes/resize.ts
@@ -1,22 +1,51 @@
 import fs from 'fs';
 import express from 'express';
 import resizer from '../imageresize';
+import { promises as fsPromises } from 'fs';
 
 const resize = express.Router();
 
-resize.get('/', async (req, res) => {
-    const fileName: string = req.query.filename as string;
-    const Width: number = parseInt(req.query.width as unknown as string);
-    const Height: number = parseInt(req.query.height as unknown as string);
-    const newImageSize: string = resizer.resizePath(fileName, Width, Height);
-    if (!fs.existsSync(newImageSize)) {
-        await resizer.resizeImage(fileName, Width, Height);
+resize.get(
+    '/',
+    async (req: express.Request, res: express.Response): Promise<void> => {
+        const fileName: string = req.query.filename as string;
+        const Width: number = parseInt(req.query.width as unknown as string);
+        const Height: number = parseInt(req.query.height as unknown as string);
+        const images = await fsPromises.readdir(`images`);
+        const dir = images.find((file) => file === fileName);
+        if (!dir) {
+            res.status(400).json({
+                message: 'Wrong file name  '
+            });
+            return;
+        }
+
+        if (!fileName) {
+            res.status(400).json({
+                message: 'Mandatory field: name is missing. '
+            });
+            return;
+        }
+        if (!Width || !Height) {
+            res.status(400).json({
+                message:
+                    'Mandatory field: Dimisnions are missing or incorrect. '
+            });
+            return;
+        }
+        const newImageSize: string = resizer.resizePath(
+            fileName,
+            Width,
+            Height
+        );
+        if (!fs.existsSync(newImageSize)) {
+            await resizer.resizeImage(fileName, Width, Height);
+        }
+        fs.readFile(newImageSize, function (err, data) {
+            res.writeHead(200);
+            res.end(data, 'binary');
+        });
     }
-    fs.readFile(newImageSize, function (err, data) {
-        if (err) res.end('Failed to Process your request');
-        res.writeHead(200);
-        res.end(data, 'binary');
-    });
-});
+);
 
 export default resize;
